b46f87aef600c6f6a63da8c65dcbb8714c2a38ba
hammad131 <74175920+hammad131@users.noreply.github.com>
2020-11-27 20:55:48 +0500

    create timer app with tdd

---
+++ src/components/App/App.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow} from 'enzyme';
+import App from './App';
+import Timer from '../Timer/Timer'
+
+describe('App Component renders', () => {
+    
+    let container = shallow(<App />)
+    
+    it('should render a div',() =>{
+        expect(container.find('div').length).toEqual(1);
+    });
+
+    it('should render the Timer component' , ()=>{
+        expect(container.containsMatchingElement(<Timer />)).toEqual(true);
+    });
+
+
+});
---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Timer from '../Timer/Timer';
+import './App.css';
+
+
+const App = () => (
+        <div className='container'>
+            <Timer />
+        </div>
+
+
+
+
+);
+
+
+
+export default App;
